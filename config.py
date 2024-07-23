60663cd7e4c16d3a8f77c99bb430f9b73a7823a9
Mayank Jaat <153971576+Mayankjat005@users.noreply.github.com>
2024-07-23 19:18:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "23159366"))
+API_HASH = getenv("API_HASH", "4623dd30dd1303bddb729eb0862262d9")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
+OWNER_ID = int(getenv("OWNER_ID", "5222155765"))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-FORCESUB = getenv("FORCESUB", "")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1001917606160"))
+FORCESUB = getenv("FORCESUB", "WarriorUnitsBots")
