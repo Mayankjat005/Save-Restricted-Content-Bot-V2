47b460bd5823b95ce08dc86f6accf0f28983a041
Mayank Jaat <153971576+Mayankjat005@users.noreply.github.com>
2024-07-23 19:37:36 +0530

    Update progress.py

--- ggn/assets/progress.py
+++ ggn/assets/progress.py
@@ -5,8 +5,8 @@ import json
 from ggn.assets.functions import TimeFormatter, humanbytes
 
 #------
-FINISHED_PROGRESS_STR = "🟢"
-UN_FINISHED_PROGRESS_STR = "🔴"
+FINISHED_PROGRESS_STR = "✅"
+UN_FINISHED_PROGRESS_STR = "❌"
 DOWNLOAD_LOCATION = "/app"
 
 
